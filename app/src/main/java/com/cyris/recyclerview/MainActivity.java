9c04156df80452560cec18a33cf8c32cf2514353
Chetan Mann <chetanmann95@gmail.com>
2017-04-02 00:18:37 +0530

    Completed the Recycler view part along with onClickListener using interface

--- app/src/main/java/com/cyris/recyclerview/MainActivity.java
+++ app/src/main/java/com/cyris/recyclerview/MainActivity.java
@@ -5,14 +5,16 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements StudentAdapter.onItemClickListener{
 
     RecyclerView recyclerView;
     StudentAdapter studentAdapter;
+    List<Student> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView=(RecyclerView)findViewById(R.id.rv_itemList);
 
-        List<Student> data=new ArrayList<>();
+        data=new ArrayList<>();
         Student student1=new Student("Chetan", "Mann");
         Student student2=new Student("Monty", "Panesar");
         data.add(student1);
@@ -41,11 +43,19 @@ public class MainActivity extends AppCompatActivity {
         data.add(student1);
         data.add(student2);
 
-        studentAdapter=new StudentAdapter(this, data);
+        studentAdapter=new StudentAdapter(data, this);
         LinearLayoutManager layoutManager=new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(studentAdapter);
+
+    }
+
+    @Override
+    public void onItemClick(int position) {
+
+        Student student=data.get(position);
+        Toast.makeText(this, "Item clicked is :"+student.firstName+"-"+student.lastName, Toast.LENGTH_SHORT).show();
 
     }
 }
--- app/src/main/java/com/cyris/recyclerview/StudentAdapter.java
+++ app/src/main/java/com/cyris/recyclerview/StudentAdapter.java
@@ -13,15 +13,21 @@ import java.util.List;
  * Created by cyris on 1/4/17.
  */
 
-public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentViewHolder>{
+public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentViewHolder> {
 
     List<Student> data;
-    Context context;
 
-    public StudentAdapter(Context context, List<Student> data)
-    {
-        this.context=context;
-        this.data=data;
+    final private onItemClickListener mOnClickListener;
+
+    public StudentAdapter(List<Student> data, onItemClickListener mOnClickListener) {
+
+        this.data = data;
+        this.mOnClickListener = mOnClickListener;
+    }
+
+
+    public interface onItemClickListener {
+        void onItemClick(int position);
     }
 
     @Override
@@ -41,7 +47,7 @@ public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentV
     @Override
     public void onBindViewHolder(StudentViewHolder holder, int position) {
 
-        Student student=data.get(position);
+        Student student = data.get(position);
         holder.bind(student);
 
     }
@@ -51,24 +57,48 @@ public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentV
         return data.size();
     }
 
-    class StudentViewHolder extends RecyclerView.ViewHolder {
+    class StudentViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView tvFirstName, tvLastName;
 
         public StudentViewHolder(View itemView) {
             super(itemView);
-            tvFirstName=(TextView)itemView.findViewById(R.id.tv_firstName);
-            tvLastName=(TextView)itemView.findViewById(R.id.tv_lastName);
+            tvFirstName = (TextView) itemView.findViewById(R.id.tv_firstName);
+            tvLastName = (TextView) itemView.findViewById(R.id.tv_lastName);
+            itemView.setOnClickListener(this);
+
+            //for setting individual click listeners :
+//            itemView.setOnClickListener(this);
+//            iconTextView.setOnClickListener(this);
+//            iconImageView.setOnLongClickListener(this);
+            //
 
         }
 
-        void bind(Student student)
-        {
+        void bind(Student student) {
             //we will bind the data here with view holder
             tvFirstName.setText(student.firstName);
             tvLastName.setText(student.lastName);
         }
 
+
+        @Override
+        public void onClick(View v) {
+
+            int clickPosition=getAdapterPosition();
+            mOnClickListener.onItemClick(clickPosition);
+//            if(v.getId()==R.id.tv_firstName)
+//            {
+//                mOnClickListener.onItemClick(data.get(clickPosition).firstName);
+//            }
+//            else
+//            {
+//                mOnClickListener.onItemClick(data.get(clickPosition).lastName);
+//            }
+
+
+
+        }
     }
 
 }
