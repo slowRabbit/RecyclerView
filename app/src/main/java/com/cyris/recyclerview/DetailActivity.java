61dcc213fc231ff2c81f189b2b6716e5e8550321
Chetan Mann <chetanmann95@gmail.com>
2017-04-03 00:39:54 +0530

    worked on pallete colors, and setting status bar and toolbar color to palette color

--- app/src/main/java/com/cyris/recyclerview/DetailActivity.java
+++ app/src/main/java/com/cyris/recyclerview/DetailActivity.java
@@ -1,15 +1,26 @@
 package com.cyris.recyclerview;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
 public class DetailActivity extends AppCompatActivity {
+
+    CollapsingToolbarLayout collapsingToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +33,7 @@ public class DetailActivity extends AppCompatActivity {
         //getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-        CollapsingToolbarLayout collapsingToolbar =
+        collapsingToolbar =
                 (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbar.setTitle("Collapsing Toolbar");
 
@@ -30,8 +41,51 @@ public class DetailActivity extends AppCompatActivity {
         Glide.with(this).load(R.drawable.john).centerCrop().into(imageView);
 
         //working for palette
-        
+        paintTextBackground();
 
+    }
+
+
+    private void paintTextBackground() {
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.john);
+
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(Palette palette) {
+
+                //work with the palette here
+                int defaultValue = 0x000000;
+                int chosenColor = palette.getDarkMutedColor(defaultValue);
+                collapsingToolbar.setContentScrimColor(chosenColor);
+
+                //changing the status bar color
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    Window window = getWindow();
+                    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                    int darkerColorChosen=getDarkerColor(chosenColor, 0.8f);
+                    window.setStatusBarColor(darkerColorChosen);
+                }
+
+            }
+        });
+
+
+    }
+
+    public static int getDarkerColor(int color, float factor) {
+
+        //user factor less than 1.0f , try 0.8f
+        int a = Color.alpha(color);
+        int r = Math.round(Color.red(color) * factor);
+        int g = Math.round(Color.green(color) * factor);
+        int b = Math.round(Color.blue(color) * factor);
+        return Color.argb(a,
+                Math.min(r,255),
+                Math.min(g,255),
+                Math.min(b,255));
     }
 
 
